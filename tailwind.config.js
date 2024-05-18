a61e1de6c43efeaf48d6fadce88c178d344aa75c
Pratiyankkumar <kumarpratiyank@gmail.com>
2024-05-18 16:03:07 +0000

    Final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './public/index.html'
+  ],
   theme: {
     extend: {},
   },
