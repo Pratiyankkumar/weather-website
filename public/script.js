522c49e239b09548c7b1bcc19c4e0ed636fc5ef3
Pratiyankkumar <kumarpratiyank@gmail.com>
2024-06-11 04:55:54 +0000

    Completed the website

--- public/script.js
+++ public/script.js
@@ -22,7 +22,14 @@ function getWeatherData(city) {
         .then(data => { 
             if (data.cod === 200) { 
                 document.getElementById('cityName').innerText = data.name;
-                document.getElementById('temperature').innerText = `Temperature: ${data.main.temp} °C`;
+                document.getElementById('temperature').innerText = `Temprature: ${data.main.temp} °C`;
+                if (data.main.temp<=15) {
+                    document.querySelector('.js-image-container')
+                        .innerHTML = `<img id="weather-image" class="md:size-96 size-60" src="amcharts_weather_icons_1.0.0/animated/snowy-5.svg" alt=""></img>`
+                } else if (data.main.temp>15 && data.main.temp<=30) {
+                    document.querySelector('.js-image-container')
+                        .innerHTML = `<img id="weather-image" class="md:size-96 size-60" src="amcharts_weather_icons_1.0.0/animated/cloudy-day-1.svg" alt=""></img>`
+                };
                 document.getElementById('description').innerText = `Description: ${data.weather[0].description}`;
                 document.getElementById('windSpeed').innerText = `Wind Speed: ${data.wind.speed} m/s`;
                 document.getElementById('humidity').innerText = `Humidity: ${data.main.humidity}%`;
@@ -46,6 +53,17 @@ function getWeatherData(city) {
 }
 
 function showResult() {
-    // document.getElementById('js-result')
-    //     .innerHTML = 
+    document.getElementById('js-result')
+        .innerHTML = `
+        <div class="js-image-container">
+        <img id="weather-image" class="md:size-96 size-60" src="amcharts_weather_icons_1.0.0/animated/day.svg" alt=""></img>
+      </div>
+      <div class="flex flex-col text-white text-center">
+        <p class="md:text-5xl text-xl" id="temperature"></p>
+        <p id="cityName" class="md:text-xl mt-2"></p>
+        <p id="description" class="md:text-xl mt-2"></p>
+        <p id="windSpeed" class="md:text-xl mt-2"></p>
+        <p id="humidity" class="md:text-xl mt-2">
+        </p>
+      </div>`
 }
